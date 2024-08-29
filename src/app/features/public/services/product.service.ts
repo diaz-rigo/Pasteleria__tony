6b889fc07ed2d51a1479d001fa818eb25a553609
meandro02 <rd209422@gmail.com>
2024-08-29 15:37:42 -0600

    prueba api render

--- src/app/features/public/services/product.service.ts
+++ src/app/features/public/services/product.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  private apiUrl = 'http://localhost:3000/product'; // Your API URL
+  private apiUrl = `${environment.api}/product`; // Your API URL
+  // return this.http.get<IproductResponse>(`${environment.api}/product/${id}`)
 
   constructor(private http: HttpClient) { }
 
