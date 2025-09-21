4aadd244730d13a2a3a13cf86aabe91f917b78fb
meandro02 <rd209422@gmail.com>
2025-09-20 22:01:56 -0600

    cambios localhos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true
-  ,api:'http://localhost:3000'
-  // ,api:'https://pasteleria-tony-back.onrender.com'
+  // ,api:'http://localhost:3000'
+  ,api:'https://pasteleria-tony-back.onrender.com'
 //
 };
